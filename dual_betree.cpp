70b7395f8f24a947073f708dfa41355b538ef8d0
shaolin-x <xshaolin123@gmail.com>
2022-03-22 16:38:32 -0400

    added query()

--- dual_betree.cpp
+++ dual_betree.cpp
@@ -6,6 +6,10 @@ DualBeTree<_Key, _Value>::DualBeTree(std::vector<std::pair<_Key, _Value>> _key_v
     // constructor
     this->key_value_pairs = _key_value_pairs;
     this->buffer_size = _buffer_size;
+    BeTree<_Key, _Value> sorted("sortedT", "./tree_dat", 4096, 10000);
+    BeTree<_Key, _Value> unsorted("unsortedT", "./tree_dat", 4096, 10000);
+    this-> sorted = sorted;
+    this-> unsorted =  unsorted;
 }
 
 template <typename _Key, typename _Value>
@@ -27,8 +31,11 @@ void DualBeTree<_Key, _Value>::build() {
 template <typename _Key, typename _Value>
 bool DualBeTree<_Key, _Value>::query(_Key key) {
     // point query
-
-    return false;
+    if(key > sorted.getMaximumKey()){
+        return this->unsorted.query(key);
+    }else{
+        return this->unsorted.query(key) || this->sorted.query(key);
+    }
 }
 
 template <typename _Key, typename _Value>
