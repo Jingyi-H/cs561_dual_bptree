0135e80bea4070b50cc5ac2695b3bd79673d5aea
Jingyi <jingyih1999@gmail.com>
2022-04-29 09:40:29 -0400

    increase the size of the bufferpool

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ std::vector<int> generatePointQueries(std::vector<int> data, int n)
 void testBptree(std::vector<int> data, std::vector<int> queries){
      // instantiate betree
     // cout<<"TEST NORAMAL B+ TREE INSERT:"<<endl;
-    BeTree<int,int> tree("manager", "./tree_dat", 4096, 10000);
+    BeTree<int,int> tree("manager", "./tree_dat", 4096, 50000);
 
     // auto insert_start1 = std::chrono::high_resolution_clock::now();
     // Loop through data and insert each element to dual bplus tree
